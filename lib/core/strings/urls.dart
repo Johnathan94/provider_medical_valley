89e645682dd7e4dfaf5a9a28c20d28430eac4718
Mohamed imbaby <mohamedimbaby999@gmail.com>
2023-07-04 19:19:24 +0300

    solve comments

--- lib/core/strings/urls.dart
+++ lib/core/strings/urls.dart
@@ -1,5 +1,5 @@
 class Strings {
   static const String baseUrl = "https://services.medvally.com/api/";
-  static const String newBaseUrl = "https://services.medvally.com/apis/";
+  static const String newBaseUrl = "https://live.services.medvally.com/apis/";
   static const String v_1 = "v1";
 }
