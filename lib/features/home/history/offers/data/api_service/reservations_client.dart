07f5f1e9426cc633909865678e69d62d9b982f0d
Mohamed imbaby <mohamedimbaby999@gmail.com>
2023-08-10 19:51:07 +0300

    solve negotiations and reservations

--- lib/features/home/history/offers/data/api_service/reservations_client.dart
+++ lib/features/home/history/offers/data/api_service/reservations_client.dart
@@ -1,19 +1,17 @@
-
-
 import 'package:dio/dio.dart';
 import 'package:provider_medical_valley/core/shared_pref/shared_pref.dart';
 import 'package:provider_medical_valley/features/auth/phone_verification/data/model/otp_response_model.dart';
 
 class ProviderReservationsClient {
-  Dio dio ;
+  Dio dio;
 
   ProviderReservationsClient(this.dio);
 
-
-  getProviderReservations( int page , int pageSize)async{
-
+  getProviderReservations(int page, int pageSize) async {
     ProviderData user = ProviderData.fromJson(LocalStorageManager.getUser()!);
-    Response response =  await dio.get("${dio.options.baseUrl}/Request/ProviderReservations?PageNumber=$page&PageSize=$pageSize&ProviderId=85",);
+    Response response = await dio.get(
+      "${dio.options.baseUrl}/Request/ProviderReservations?PageNumber=$page&PageSize=$pageSize&ProviderId=${user.id}",
+    );
     return response.data;
   }
 }
