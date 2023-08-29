8ca309c4b6fb15b57cfb0dea180e2592fa1ca641
Mohamed imbaby <mohamedimbaby999@gmail.com>
2023-08-29 09:43:40 +0300

    upgrade version to be 1.0.3

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,5 +1,5 @@
 class AppSettings {
-  static String appVersion = "1.0.2";
+  static String appVersion = "1.0.3";
   static String description =
       "-#154 fix Refresh in Provider's homepage ,#142 remove rating from send offer and home screen";
 }
