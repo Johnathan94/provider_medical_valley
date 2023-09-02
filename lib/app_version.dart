5e2955aef7377304bc32950cb57a44dfed2b1e20
elkholy <boodykh@gmail.com>
2023-09-02 23:04:36 +0300

    updated app version

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,4 +1,5 @@
 class AppSettings {
-  static String appVersion = "1.0.4";
-  static String description = "-migration to alpha and fixing issue";
+  static String appVersion = "1.0.5";
+  static String description =
+      "-169-171--remove user phone from negotiate card and solved translation of insurance options";
 }
