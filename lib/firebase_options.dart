c24e55cf99d5881e1105a6560f42e6ffa9e9d9c5
elkholy <boodykh@gmail.com>
2023-09-06 23:53:54 +0300

    run cli and notifications entitlemnents

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAByIYX3TRM5ZmvMeL00gg3hQTJRQpGV3w',
-    appId: '1:600040989984:android:dad2efd9b4a87cd7d16345',
+    appId: '1:600040989984:android:5e47324107456936d16345',
     messagingSenderId: '600040989984',
     projectId: 'medvalley-ff30c',
     storageBucket: 'medvalley-ff30c.appspot.com',
